0aca6fc55142ff749bc10d1849ed21eb64e5289e
BartlomiejB <blaszczyk17@gmail.com>
2025-08-07 00:02:15 +0200

    feature#1: Basic return logic impl.

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class ReturnDeviceFromUser
-  def initialize
-    # TODO
+
+  def initialize(user:, serial_number:, from_user_id:)
+    @user = user
+    @serial_number = serial_number
+    @from_user_id = from_user_id
   end
 
   def call
-    # TODO
+    device = Device.find_by(serial_number: serial_number)
+    return unless device
+
+    if device.user_id == from_user_id
+      device.update!(user_id: nil)
+    end
   end
+
+  private attr_reader :user, :serial_number, :from_user_id
 end
